9a47379024bdb2cbb61264bc89a21a40f1d0a522
Cyrus Jia <cjia@usc.edu>
2016-02-11 12:34:56 -0800

    Working Timer 2

--- welcome.js
+++ welcome.js
@@ -1,3 +1,22 @@
-/**
- * Created by Cyrus on 2/4/16.
- */
+
+var start = 0;
+function startTimer(duration, display) {
+    start = duration;
+
+    function timer() {
+        if(start>0) start--;
+
+        display.textContent = "" + start;
+
+    }
+    // we don't want to wait a full second before the timer starts
+    timer();
+    setInterval(timer, 1000);
+}
+
+function buttonClicked(){
+    start = 100;
+    var display = document.querySelector('#counter');
+    startTimer(start, display);
+
+}
