d42796a793aa64cc2a0ccb539bb4bb38144a6350
Cyrus Jia <cyrus.jia@gopuff.com>
2021-06-08 12:21:01 -0500

    p change

--- particles.js
+++ particles.js
@@ -78,9 +78,9 @@ var pJS = function(tag_id, params){
         random: false,
         straight: false,
         out_mode: 'out',
-        bounce: false,
+        bounce: true,
         attract: {
-          enable: false,
+          enable: true,
           rotateX: 3000,
           rotateY: 3000
         }
@@ -88,7 +88,7 @@ var pJS = function(tag_id, params){
       array: []
     },
     interactivity: {
-      detect_on: 'window',
+      detect_on: 'canvas',
       events: {
         onhover: {
           enable: true,
